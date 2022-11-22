a6bf7c175a108a3b5068ee5d8bd2bc185e96a10b
soabolaji <soabolaji@gmail.com>
2022-11-22 07:08:04 -0800

    handle

--- main.h
+++ main.h
@@ -3,11 +3,54 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
+
+/**
+ * struct flag_b - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
+ */
+typedef struct flag_b
+{
+	unsigned char flag;
+	unsigned char value;
+} flag_k;
+
+/**
+ * struct buffer_b - new type defining a buffer struct.
+ * @buffer: pointer to a character array.
+ * @start: pointer to the start of buffer.
+ * @length: length of the string stored in buffer.
+ */
+typedef struct buffer_b
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_k;
+
 
 /**
  * struct conv - a structure for symbols and functions
  * @sym: a symbol
- * @func: function associated with the symbol
+ * @func: function associated with the system
  */
 
 typedef struct conv
@@ -37,6 +80,12 @@ int printf_octal(va_list list);
 int printf_hex_lower(va_list list);
 int printf_hex_upper(va_list list);
 int printf_addressp(va_list list);
+
+/*handle modifiers*/
+unsigned char h_flags(const char *flag, char *f);
+unsigned char h_length(const char *mod, char *f);
+int h_width(va_list para, const char *mod, char *f);
+int h_precision(va_list para, const char *mod, char *f);
 
 
 #endif /* _MAIN_H */
---
+++ mod_hand.c
@@ -0,0 +1,148 @@
+#include "main.h"
+
+
+unsigned char h_flags(const char *flag, char *f);
+unsigned char h_length(const char *mod, char *f);
+int h_width(va_list para, const char *mod, char *f);
+int h_precision(va_list para, const char *mod, char *f);
+
+/**
+ * h_flags - handle the flag with its value
+ * @flag: the flag to the strings
+ * @f: pointer to the string
+ * Return: if the character match value otherwise 0
+ */
+
+unsigned char h_flags(const char *flag, char *f)
+{
+	int i, cot;
+	unsigned char lag = 0;
+	flag_k flags[] = {
+		{'+', PLUS},
+		{' ', SPACE},
+		{'#', HASH},
+		{'0', ZERO},
+		{'-', NEG},
+		{0, 0}
+	};
+
+	for (i = 0; flag[i]; i++)
+	{
+	for (cot = 0; flags[cot].flag != 0; cot++)
+	{
+	if (flag[i] == flags[cot].flag)
+	{
+	(*f)++;
+	if (lag == 0)
+	lag = flags[cot].value;
+	else
+	lag |= flags[cot].value;
+	break;
+	}
+	}
+	if (flags[cot].value == 0)
+	break;
+	}
+
+	return (lag);
+}
+
+/**
+ * h_length - to display the length of a string
+ * @mod: a modifier pointer
+ * @f: the index of the string
+ * Return: the length modifier if sucess
+ */
+
+unsigned char h_length(const char *mod, char *f)
+{
+	if (*mod == 'h')
+	{
+		(*f)++;
+		return (SHORT);
+	}
+
+	else if (*mod == 'l')
+	{
+		(*f)++;
+		return (LONG);
+	}
+
+	return (0);
+}
+
+/**
+ * h_width - check the modifier with corresponding value
+ * @mod: modifier pointer
+ * @f: a counter of the format string
+ * Return: display the width otherwise 0
+ */
+
+int h_width(va_list para, const char *mod, char *f)
+{
+	int value = 0;
+
+	while ((*mod >= '0' && *mod <= '9') || (*mod == '*'))
+	{
+	(*f)++;
+
+	if (*mod == '*')
+	{
+	value = va_arg(para, int);
+	if (value <= 0)
+	return (0);
+	return (value);
+	}
+
+	value *= 10;
+	value += (*mod - '0');
+	mod++;
+	}
+
+	return (value);
+}
+
+/**
+ * h_precision - relate the modifier withthe result value
+ * @para: the argument parameter
+ * @mod: the string modifier
+ * @f: a counter of the foemat string
+ * Return: if match the value, if empty zero or neg otherwise 0
+ */
+
+int h_precision(va_list para, const char *mod, char *f)
+{
+	int value = 0;
+
+	if (*mod != '.')
+	return (-1);
+
+	mod++;
+	(*f)++;
+
+	if ((*mod <= '0' || *mod > '9') && *mod != '*')
+	{
+	if (*mod == '0')
+	(*f)++;
+	return (0);
+	}
+
+	while ((*mod >= '0' && *mod <= '9') || (*mod == '*'))
+	{
+	(*f)++;
+
+	if (*mod == '*')
+	{
+	value = va_arg(para, int);
+	if (value <= 0)
+	return (0);
+	return (value);
+	}
+
+	value *= 10;
+	value += (*mod - '0');
+	mod++;
+	}
+
+	return (value);
+}
