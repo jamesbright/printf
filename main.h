59cf9196fcc53116ae8260c0e181865c90198bef
soabolaji <soabolaji@gmail.com>
2022-11-22 09:30:16 -0800

    fix

--- main.h
+++ main.h
@@ -26,12 +26,26 @@
  * struct flag_b - A new type defining a flags struct.
  * @flag: A character representing a flag.
  * @value: The integer value of the flag.
- */
+ *
 typedef struct flag_b
 {
 	unsigned char flag;
 	unsigned char value;
-} flag_k;
+} flag_k;*/
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
 /**
  * struct buffer_b - new type defining a buffer struct.
